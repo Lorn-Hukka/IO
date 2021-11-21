70acfe8a3ab369a8e31a16a783016e6280ed301c
Lorn Hukka <50033807+Lorn-Hukka@users.noreply.github.com>
2021-11-21 17:05:06 +0100

    Dodano podstawowe informacje do head'a;
    - Autorzy
    - Logo
    - Opis
    - Tytuł

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,19 @@ import Head from 'next/head'
 const Layout = ({ children }) => {
     return (
         <>
-
+            <Head>
+                {/* <link rel="shortcut icon" href="/favicon.ico" /> */}
+                <link rel="icon" href="logo.svg"></link>
+                <meta name="author" content="Wojciech Kasolik / Błażej Mrzygłód / Radosław Koczur" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta
+                    name="description"
+                    content='Sklep internetowy "Sagittarius" dla zapalonych pasjonatów Fantastyki.
+                            To tutaj dostaniesz wszystko co potrzebne do dobrej zabawy; 
+                            Planszówki, kostki, podręczniki do RPGów i wiele więcej.
+                ' />
+                <title>Sagittarius - Twój sklep hobbystyczny.</title>
+            </Head>
             <div className="flex flex-col min-h-screen overflow-hidden">
                 {/* <Header /> */}
                 <main className="flex-grow">
