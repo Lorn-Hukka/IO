ac995842a060107aa0918b22ccbf3be474287743
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2021-11-21 17:55:18 +0100

    Dodanie ikonki "strzałki" do menu "kategorie"

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { Menu } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
 
 
 const Header = () => {
     return (  
-        <nav>
-            
+        <nav> 
             <Link href="/"><a>
                 <Image src="/logo-right.svg" width ={256} height={128} />
             </a></Link>
+            <div className="searchBar inline-block">
             <input type="text" placeholder="Szukaj..." />
             <Menu>
-            <Menu.Button>Kategorie</Menu.Button>
+            <Menu.Button>Kategorie  <ChevronDownIcon className="w-5 h-5 ml-2 -mr-1" /> </Menu.Button>
             <Menu.Items>
               <Menu.Item>
                 {({ active }) => (
                   <a
                     className={`${active && 'bg-blue-500'}`}
-                    href="/account-settings"
+                    href="/"
                   >
                     Lorem ipsum
                   </a>
@@ -29,7 +30,7 @@ const Header = () => {
                 {({ active }) => (
                   <a
                     className={`${active && 'bg-blue-500'}`}
-                    href="/account-settings"
+                    href="/"
                   >
                     Lorem ipsum
                   </a>
@@ -37,6 +38,7 @@ const Header = () => {
               </Menu.Item>
             </Menu.Items>
           </Menu>
+          </div>
             <Link href="/"><a>Kontakt</a></Link>
             <Link href="/"><a>Twoje konto</a></Link>
             <Link href="/"><a>Koszyk</a></Link>
