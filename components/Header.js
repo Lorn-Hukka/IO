d3b5697713b8290ce4a57251ba959efd0e998d15
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2021-11-21 17:41:37 +0100

    Dodanie rozwijanego menu "kategorie" do paska wyszukiwania

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,42 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { Menu } from '@headlessui/react'
+
+
 
 const Header = () => {
     return (  
         <nav>
+            
             <Link href="/"><a>
-                <Image src="/logo-right.svg" width ={128} height={77} />
+                <Image src="/logo-right.svg" width ={256} height={128} />
             </a></Link>
             <input type="text" placeholder="Szukaj..." />
+            <Menu>
+            <Menu.Button>Kategorie</Menu.Button>
+            <Menu.Items>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && 'bg-blue-500'}`}
+                    href="/account-settings"
+                  >
+                    Lorem ipsum
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && 'bg-blue-500'}`}
+                    href="/account-settings"
+                  >
+                    Lorem ipsum
+                  </a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
             <Link href="/"><a>Kontakt</a></Link>
             <Link href="/"><a>Twoje konto</a></Link>
             <Link href="/"><a>Koszyk</a></Link>
