5114355238ee1f3eb72abc4713b149675b674170
Lorn Hukka <50033807+Lorn-Hukka@users.noreply.github.com>
2021-11-28 05:13:32 +0100

    Zmieniono kolorystykę i iconę na lupkę.

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SearchIcon } from '@heroicons/react/outline'
 
 const ProductCard = ({ product }) => {
   return (
@@ -6,10 +7,11 @@ const ProductCard = ({ product }) => {
       <Link href={"/products/" + product.slug}>
         <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
           <div className="flex items-end justify-end h-56 w-full bg-cover" style={{ backgroundImage: "url(" + product.thumbnail + ")" }}>
-            <button className="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-              <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+            <button className="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
+              <SearchIcon className="h-5 w-5" aria-hidden="true" />
+              {/* <svg className="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-              </svg>
+              </svg> */}
             </button>
           </div>
           <div className="px-5 py-3">
