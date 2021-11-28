b8bd80afe914d7926a8850a42dab5f879a947ed4
Radosław Koczur <49763978+Radek337@users.noreply.github.com>
2021-11-28 21:07:39 +0100

    korekta błędów stylistycznych i składniowych

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
   <div className="container mx-auto px-4">
     <div className="flex flex-wrap text-left lg:text-left">
       <div className="w-full lg:w-6/12 px-4">
-        <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+        <h4 className="text-3xl fonat-semibold text-blueGray-700">Zostańmy w kontakcie!</h4>
         <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-          Find us on any of these platforms, we respond 1-2 business days.
+          Możesz obserwowac nasze konta na aż 4 platformach społecznościowych.
         </h5>
         <div className="mt-6 lg:mb-0 mb-6">
           <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
@@ -24,16 +24,16 @@ const Footer = () => {
             <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Przydatne linki</span>
             <ul className="list-unstyled">
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Twoje konto</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Twoje konto</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Dostsawa</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Dostsawa</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Zwroty/Reklamacje</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Zwroty/Reklamacje</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Wspólpraca</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Wspólpraca</a>
               </li>
             </ul>
           </div>
@@ -41,16 +41,16 @@ const Footer = () => {
             <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Kontakt</span>
             <ul className="list-unstyled">
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">Radosław Wojciech Koczur S.A. Sadowa 13</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">Sagittarius S.A. Sadowa 13</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">48 000 000 000</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">48 000 000 000</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">nazwaMaila@gmail.com</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">nazwaMaila@gmail.com</a>
               </li>
               <li>
-                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="">nazwaMail2a@gmail.com</a>
+                <a className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="#">nazwaMail2a@gmail.com</a>
               </li>
             </ul>
           </div>
@@ -61,9 +61,7 @@ const Footer = () => {
     <div className="flex flex-wrap items-center md:justify-between justify-center">
       <div className="w-full md:w-4/12 px-4 mx-auto text-center">
         <div className="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span>
-          <a href="" className="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by</a>
-          <a href="" className="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>.
+          Sagittarius© Wszystkie prawa zastrzerzone  <span id="get-current-year">2021</span>
         </div>
       </div>
     </div>
