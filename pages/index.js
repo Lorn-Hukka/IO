4d829c7814461b008aef691a131ebb2847709967
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2021-12-10 19:58:45 +0100

    Dodanie do strony głównej sekcji "O tej stronie"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <h1>Projekt startowy.</h1>
+    <div className="container mx-auto">
+      <div className="container flex flex-wrap">
+        <div>
+          <h1 className="text-gray-900 text-2xl font-bold">O tej stronie</h1>
+          <span>Na naszej stronie znajdziesz wszystko co jest związane z grą w papierowe gry fantasy! <br/>
+          Wszystko co jest Ci potrzebne jesteś w stanie nabyć właśnie tu!</span>
+        </div>
+      </div> 
+    </div>
   )
 }
