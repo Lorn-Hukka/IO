d98ad87f6b9ad085a51e6fd37588e0bdc7eed52b
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2021-12-10 20:09:06 +0100

    Dodanie do strony głównej sekcji z produktami polecanymi oraz na promocji

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import ProductCard from "@components/products/ProductCard"
 export default function Home() {
+
+  let dummyData = [
+    {"slug": "podrecznik-darkherezy-ii", "name": "Podręcznik DarkHerezy II", "price": "179,00 zł", "thumbnail": "https://copcorp.pl/wp-content/uploads/2018/12/dh2-1.jpg"},
+    {"slug": "zestaw-kosci-do-gry-hellman", "name": "Zestaw Kości do gry - HELLMAN", "price": "30 zł", "thumbnail": "https://files.rebel.pl/products/107/5904/5382/5383/_2005372/0-1200x900-ffffff.jpg"},
+  ];
+
   return (
     <div className="container mx-auto">
       <div className="container flex flex-wrap">
@@ -7,6 +14,28 @@ export default function Home() {
           <span>Na naszej stronie znajdziesz wszystko co jest związane z grą w papierowe gry fantasy! <br/>
           Wszystko co jest Ci potrzebne jesteś w stanie nabyć właśnie tu!</span>
         </div>
+        <div className="">
+          <h1 className="text-gray-900 text-2xl font-bold">Polecamy</h1>
+          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+              {
+                dummyData && dummyData.map( product => (
+                  <ProductCard key={product.slug} product={product} />
+                  )
+                )
+              }
+            </div>
+          </div>
+          <div>
+            <h1 className="text-gray-900 text-2xl font-bold">Promocje</h1>
+            <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
+              {
+                dummyData && dummyData.map( product => (
+                  <ProductCard key={product.slug} product={product} />
+                  )
+                )
+              }
+            </div>
+          </div>
       </div> 
     </div>
   )
