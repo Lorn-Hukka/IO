c787cfdf80347ded9d7e93c007074c9f454ee734
Radosław Koczur <49763978+Radek337@users.noreply.github.com>
2021-12-12 20:53:52 +0100

    stworzenie struktury dla latającego koszyka

---
+++ components/FlyingCart.js
@@ -0,0 +1,14 @@
+const Cart = () => {
+    return ( 
+        <>
+    <div className="flyingCartWrapper">
+        <div className="flyingCart">
+            <a href="#"><i className="fas fa-shopping-cart"></i></a>
+            <div className="counter">3</div>
+        </div>  
+    </div>
+        </>
+     );
+}
+ 
+export default Cart;
