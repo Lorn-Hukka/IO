6f1dc4d0c30a3f5115ed5cca1a216d3d453f15ee
Lorn Hukka <50033807+Lorn-Hukka@users.noreply.github.com>
2021-12-19 21:12:01 +0100

    Zmienio dummy data na produkty z bazy.

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,21 +1,34 @@
 import ProductCard from "@components/products/ProductCard"
 
-const Products = () => {
+export async function getStaticProps(context) {
 
-  let dummyData = [
-    {"slug": "podrecznik-darkherezy-ii", "name": "Podręcznik DarkHerezy II", "price": "179,00 zł", "thumbnail": "https://copcorp.pl/wp-content/uploads/2018/12/dh2-1.jpg"},
-    {"slug": "zestaw-kosci-do-gry-hellman", "name": "Zestaw Kości do gry - HELLMAN", "price": "30 zł", "thumbnail": "https://files.rebel.pl/products/107/5904/5382/5383/_2005372/0-1200x900-ffffff.jpg"},
-  ];
+  let res = await fetch("https://cms.io.lorned.net/products");
+  let products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 900
+  }
+}
+
+const Products = ({ products }) => {
+
+  // let dummyData = [
+  //   {"slug": "podrecznik-darkherezy-ii", "name": "Podręcznik DarkHerezy II", "price": "179,00 zł", "thumbnail": "https://copcorp.pl/wp-content/uploads/2018/12/dh2-1.jpg"},
+  //   {"slug": "zestaw-kosci-do-gry-hellman", "name": "Zestaw Kości do gry - HELLMAN", "price": "30 zł", "thumbnail": "https://files.rebel.pl/products/107/5904/5382/5383/_2005372/0-1200x900-ffffff.jpg"},
+  // ];
 
   return (
     <>
       <div className="my-8">
         <div className="container mx-auto px-6">
           <h3 className="text-gray-700 text-2xl font-medium">Nasze Produkty:</h3>
-          <span className="mt-3 text-sm text-gray-500">{dummyData.length} Produktów</span>
+          <span className="mt-3 text-sm text-gray-500">{products.length} Produktów</span>
           <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
             {
-              dummyData && dummyData.map( product => (
+              products && products.map( product => (
                 <ProductCard key={product.slug} product={product} />
                 )
               )
