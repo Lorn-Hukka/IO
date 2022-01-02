cdc243d426042f1b08489781a41d00a50ebceda1
Lorn Hukka <50033807+Lorn-Hukka@users.noreply.github.com>
2022-01-02 03:47:44 +0100

    Usunięto komentarz ze starymi dummy data.

--- pages/products/index.js
+++ pages/products/index.js
@@ -15,11 +15,6 @@ export async function getStaticProps(context) {
 
 const Products = ({ products }) => {
 
-  // let dummyData = [
-  //   {"slug": "podrecznik-darkherezy-ii", "name": "Podręcznik DarkHerezy II", "price": "179,00 zł", "thumbnail": "https://copcorp.pl/wp-content/uploads/2018/12/dh2-1.jpg"},
-  //   {"slug": "zestaw-kosci-do-gry-hellman", "name": "Zestaw Kości do gry - HELLMAN", "price": "30 zł", "thumbnail": "https://files.rebel.pl/products/107/5904/5382/5383/_2005372/0-1200x900-ffffff.jpg"},
-  // ];
-
   return (
     <>
       <div className="my-8">
