c6d8368ee52cd5aca7d99eea9ddfc8100a34c513
Lorn Hukka <50033807+Lorn-Hukka@users.noreply.github.com>
2022-01-02 03:46:43 +0100

    Podmiana dummy data na faktyczne dane.
    - Zmieniono dummy data na dane pobierane z api;
    - Usunięto nieistniejący komponent SaleProductCard (Funkcja przejęta przez ProductCard);
    - Zmieniono nazwę sekcji "Polecamy" na "Nowości";

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import ProductCard from "@components/products/ProductCard"
-import SaleProductCard from "@components/products/SaleProductCard"
-export default function Home() {
 
-  let dummyData = [
-    {"slug": "podrecznik-darkherezy-ii", "name": "Podręcznik DarkHerezy II", "price": "179,00 zł", "salePrice": "100,00 zł", "thumbnail": "https://copcorp.pl/wp-content/uploads/2018/12/dh2-1.jpg"},
-    {"slug": "zestaw-kosci-do-gry-hellman", "name": "Zestaw Kości do gry - HELLMAN", "price": "30,00 zł", "salePrice": "25,00 zł", "thumbnail": "https://files.rebel.pl/products/107/5904/5382/5383/_2005372/0-1200x900-ffffff.jpg"},
-  ];
+// https://cms.io.lorned.net/products?on_sale=1&_limit=5
+// https://cms.io.lorned.net/products?_sort=id:DESC&_limit=5
+
+export async function getStaticProps(context){
+
+  let res = await fetch("https://cms.io.lorned.net/products?on_sale=true&_limit=3");
+  let on_sale = await res.json();
+
+  res = await fetch("https://cms.io.lorned.net/products?_sort=id:DESC&_limit=3");
+  let fresh = await res.json();
+
+  return {
+    props: {
+      on_sale,
+      fresh,
+    },
+    revalidate: 3600
+  }
+}
+
+
+export default function Home({on_sale, fresh}) {
 
   return (
-      <div className="">
+      <>
         <div className="border-b-2">
         <div className="px-6 py-10">
           <h1 className="text-gray-700 text-2xl font-medium mb-2">O tej stronie</h1>
@@ -18,10 +34,10 @@ export default function Home() {
         </div>
         <div className="border-b-2">
         <div className="px-6 py-10">
-          <h1 className="text-gray-700 text-2xl font-medium">Polecamy</h1>
+          <h1 className="text-gray-700 text-2xl font-medium">Nowości</h1>
           <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
               {
-                dummyData && dummyData.map( product => (
+                fresh && fresh.map( product => (
                   <ProductCard key={product.slug} product={product} />
                   )
                 )
@@ -33,15 +49,15 @@ export default function Home() {
           <div className="px-6 py-10">
             <h1 className="text-gray-700 text-2xl font-medium">Promocje</h1>
             <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-              {
-                dummyData && dummyData.map( product => (
-                  <SaleProductCard key={product.slug} product={product} />
+            {
+                on_sale && on_sale.map( product => (
+                  <ProductCard key={product.slug} product={product} />
                   )
                 )
               }
             </div>
           </div>
           </div>
-      </div> 
+      </> 
   )
 }
