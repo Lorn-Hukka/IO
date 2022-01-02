ae49fb3e394c233650c263d6a6f34c0dfea09161
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2022-01-02 20:08:13 +0100

    Stylizacja CartItem

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,21 +1,25 @@
 import { TrashIcon } from '@heroicons/react/outline'
 import { HeartIcon } from '@heroicons/react/outline'
+import Image from 'next/dist/client/image';
 const CartItem = ({ product }) => {
     return ( 
-        <div className="flex items-center">
-            <div>
-                Zdjęcie
-            </div>
+        <div className="grid grid-cols-2 justify-items-center">
+            <div className='flex items-center'>
+                <div className='m-5'>
+                    <Image src="/logo.svg" alt="logo" width ={64} height={64} /> {/*placeholder*/}
+                </div>
 
-            <div>
-                {product.name}
+                <div className=''>
+                    {product.name}
+                </div>
             </div>
                 
-            <div>
+            <div className='m-3'>
                 {product.price}
+            
+                <HeartIcon className="my-1 h-5 w-5 hover:bg-gray-200 hover:cursor-pointer"/>
+                <TrashIcon className="my-1 h-5 w-5 hover:bg-gray-200 hover:cursor-pointer"/>
             </div>
-            <HeartIcon className="h-5 w-5"/>
-            <TrashIcon className="h-5 w-5"/>
             
         </div>
      );
