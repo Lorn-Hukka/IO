11dc39f756765952d6d7349e4628ebfacefa4a98
BlazejMrzyglod <89865090+BlazejMrzyglod@users.noreply.github.com>
2022-01-24 01:36:08 +0100

    Refactoring funckji "converter"

--- converter.js
+++ converter.js
@@ -1,4 +1,7 @@
-function converter (a, b)  {
-    return a*b;
+function converter (a = 1, b = 4.4)  {
+    if (a == 0 || b == 0) {
+        return 'Żaden z parametrów nie może być równy 0'
+    }
+    return a * b;
 }
 module.exports = converter;
